0d64711846778d5397faac2f8c6414db93e28abb
MrLazzyboy <msrlazzyboy@gmail.com>
2018-08-01 18:03:49 +0300

    'styles'

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-one-expression-per-line,jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
 import './App.css';
@@ -31,78 +34,125 @@ class App extends Component {
           active_card: '----',
         },
       ],
-    }
+    };
   }
 
-clickHandler() {
-    console.log(`it was a pen`);
-}
+  clickHandler() {
+    console.log('it was a pen');
+  }
 
-clickHandlerSecond() {
-        console.log(`it was a close tab`);
-    }
+  clickHandlerSecond() {
+    console.log('it was a close tab');
+  }
 
   render() {
-  const { users } = this.state;
+    const { users } = this.state;
 
     return (
       <div className="App">
-        <div className='main-table'>
-          <div className='col-4 text-left'>
-            <h3>Клиенты</h3><br/>
-            <span className="row"><i className="fas fa-plus-circle mr-2 ml-2"/><p>Добавить клиента</p></span>
+        <div className="main-table">
+          <div className="col-4 text-left">
+            <h3 className="header-form">
+Клиенты
+            </h3>
+            <span className="row">
+              <i className="fas fa-plus-circle mr-2 ml-2 add-icon" />
+              <p className="add">
+Добавить клиента
+              </p>
+            </span>
           </div>
-          <div className="row col-12">
-              <div className="col-6">
-                  Имя<input type="text"/>
+          <div className="modal-tab">
+            <div className="row col-12">
+              <div className="col-7">
+                <div className="form-group">
+                  <label className="label">Имя</label>
+                  <input
+                    className="form-control border__input"
+                    type="text"
+                    placeholder="ex. Mr. Anderson"
+                  />
+                </div>
               </div>
-              <div className="col-6">
-                  Телефон<input type="text"/>
+              <div className="col-5">
+                <div className="form-group">
+                  <label className="label">Телефон</label>
+                  <input
+                    className="form-control border__input"
+                    type="text"
+                    placeholder="ex. 8 999 888 77 66"
+                  />
+                </div>
               </div>
+            </div>
+            <div className="row col-12">
+              <div className="col-7">
+                <div className="form-group">
+                  <label className="label">E-Mail</label>
+                  <input
+                    className="form-control border__input"
+                    type="text"
+                    placeholder="ex. example@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="col-5">
+                <button type="button" className="btn-save">
+                  Сохранить
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="row col-12">
-              <div className="col-6">
-                  E-mail<input type="text"/>
-              </div>
-              <div className="col-6">
-                  <button type="button">Сохранить</button>
-              </div>
-          </div>
+          <div className="pl-3 pr-3">
+            <hr />
             <table className="table table-borderless col-12 ">
-          <thead>
-            <tr>
-              <th className="col-2"/>
-              <th className="col-2">Клиент</th>
-              <th className="col-2">Телефон</th>
-              <th className="col-2">Email</th>
-              <th className="col-1">Дата последнего посещения</th>
-              <th className="col-1">Сумма оплат</th>
-              <th className="col-1">Колличество посещений</th>
-              <th className="col-1">Активный абонемент</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map( row =>
-            (
-              <tr key={row.id}>
-                <td>
-                  <div className="col">
-                      <i className="fas fa-pen mr-2" onClick={this.clickHandler} />
-                      <i className="fas fa-times" onClick={this.clickHandlerSecond} />
-                  </div>
-                </td>
-                <td>{row.username}</td>
-                <td>{row.phone}</td>
-                <td>{row.email}</td>
-                <td>{row.last_visit}</td>
-                <td>{row.summ_of_payments}</td>
-                <td>{row.summ_of_visits}</td>
-                <td>{row.active_card}</td>
-              </tr>
-            )
-            )}
-          </tbody>
-        </table>
+              <thead>
+                <tr>
+                  <th className="th-own" />
+                  <th className="th-own">Клиент</th>
+                  <th className="th-own">Телефон</th>
+                  <th className="th-own">Email</th>
+                  <th className="th-own">Дата последнего посещения</th>
+                  <th className="th-own">Сумма оплат</th>
+                  <th className="th-own">Колличество посещений</th>
+                  <th className="th-own">Активный абонемент</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map(row => (
+                  <tr key={row.id}>
+                    <td align="left">
+                      <div className="col d-inline-flex">
+                        <i className="fas fa-pen mr-2" onClick={this.clickHandler} />
+                        <i className="fas fa-times" onClick={this.clickHandlerSecond} />
+                      </div>
+                    </td>
+                    <td className="table-font">
+                      {row.username}
+                    </td>
+                    <td className="table-font">
+                      {row.phone}
+                    </td>
+                    <td className="table-font">
+                      {row.email}
+                    </td>
+                    <td className="table-font">
+                      {row.last_visit}
+                    </td>
+                    <td className="table-font">
+                      {row.summ_of_payments}
+                    </td>
+                    <td className="table-font">
+                      {row.summ_of_visits}
+                    </td>
+                    <td className="table-font">
+                      {row.active_card}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
