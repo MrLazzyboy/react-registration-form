31052beee00e9a9350aa11ee472faa905c3cbcd5
MrLazzyboy <msrlazzyboy@gmail.com>
2018-08-06 21:03:55 +0300

    added event handlers and fixed styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events,react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/click-events-have-key-events,react/jsx-one-expression-per-line,no-unused-vars,react/no-access-state-in-setstate,react/destructuring-assignment,max-len */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
@@ -34,32 +34,107 @@ class App extends Component {
           active_card: '----',
         },
       ],
+      isOpen: false,
+      editUser: {
+        id: null,
+        username: '',
+        phone: '',
+        email: '',
+      },
     };
   }
 
-  clickHandler() {
-    console.log('it was a pen');
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
   }
+  // вкл/выкл отображения компонента
 
-  clickHandlerSecond() {
-    console.log('it was a close tab');
+  editHandler(id) {
+    return (event) => {
+      const newState = this.state;
+      const user = newState.users.find(item => item.id === id);
+      this.setState({ editUser: user });
+      this.setState({ isOpen: true });
+    };
   }
+  // обработчик редактирования
+
+  deleteHandler(id) {
+    return (event) => {
+      const newState = this.state;
+      const user = newState.users.find(item => item.id === id);
+      newState.users.splice(newState.users.indexOf(user), 1);
+      this.setState(newState);
+    };
+  }
+  // обработчик удаления
+
+  closeIconDisplay() {
+    return (
+      this.state.isOpen ? <i className="fas fa-times add-icon pointer" onClick={this.toggle} /> : null
+    );
+  }
+  // отображение иконки крестика
+
+  updateData(value) {
+    const newState = this.state;
+
+    if (value.id === null) {
+      const newUser = {
+        ...value,
+        id: this.idGenerator(),
+        last_visit: '----',
+        summ_of_payments: '----',
+        summ_of_visits: '----',
+        active_card: '----',
+      };
+      newState.users.push(newUser);
+    } else {
+      const oldUser = newState.users.find(item => item.id === value.id);
+      newState.users.splice(newState.users.indexOf(oldUser), 1, value);
+    }
+
+    this.setState(newState);
+    this.setState({
+      editUser: {
+        id: null,
+        username: '',
+        phone: '',
+        email: '',
+      },
+    });
+  }
+  // колбэк для возврата информации из компонента ввода
+
+  idGenerator() {
+    return `_${Math.random().toString(36).substr(2, 9)}`;
+  }
+  // генерируем уникальный id
 
   render() {
     const { users } = this.state;
-
     return (
       <div className="App">
         <div className="main-table">
-          <div className="col-4 text-left">
-            <h3 className="header-form">Клиенты</h3>
-            <span className="row">
-              <i className="fas fa-plus-circle mr-2 ml-2 add-icon" />
-              <p className="add">Добавить клиента</p>
+          <div className="row text-left">
+            <div className="col-4 ml-4">
+              <h3 className="header-form">Клиенты</h3>
+              <span className="row">
+                <i className="fas fa-plus-circle mr-2 ml-2 add-icon pointer" onClick={this.toggle} />
+                <p className="add">Добавить клиента</p>
+              </span>
+            </div>
+            <span className="col text-right mr-4 mt-4">
+              {this.closeIconDisplay()}
             </span>
           </div>
           <div className="pl-3 pr-3">
-            {/*<InputForm />*/}
+            <InputForm
+              isOpen={this.state.isOpen}
+              editUser={this.state.editUser}
+              updateData={this.updateData}
+              toggle={this.toggle}
+            />
             <hr />
             <table className="table table-borderless col-12 ">
               <thead>
@@ -79,8 +154,8 @@ class App extends Component {
                   <tr key={row.id}>
                     <td align="left">
                       <div className="col d-inline-flex">
-                        <i className="fas fa-pen mr-2" onClick={this.clickHandler} />
-                        <i className="fas fa-times" onClick={this.clickHandlerSecond} />
+                        <i className="fas fa-pen mr-2 pointer" onClick={this.editHandler(row.id)} />
+                        <i className="fas fa-times pointer" onClick={this.deleteHandler(row.id)} />
                       </div>
                     </td>
                     <td className="table-font">
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,29 +1,66 @@
-/* eslint-disable jsx-a11y/label-has-for,react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/label-has-for,react/jsx-one-expression-per-line,react/destructuring-assignment,max-len */
 import React, { Component } from 'react';
+import autoBind from 'react-autobind';
 import './App.css';
 
 class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+
+    this.state = this.props.editUser;
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState(newProps.editUser);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  } // обработчик изменений
+
+  handleSubmit() {
+    this.props.updateData(this.state);
+    this.setState({
+      username: '',
+      email: '',
+      phone: '',
+    });
+    this.props.toggle();
+  } // обработчик сохранения
+
   render() {
+    if (!this.props.isOpen) return null;
+
     return (
       <div className="modal-tab">
         <div className="row col-12">
           <div className="col-7">
             <div className="form-group">
-              <label className="label">Имя</label>
+              <label className="label" htmlFor="username">Имя</label>
               <input
+                id="username"
+                name="username"
                 className="form-control border__input"
                 type="text"
                 placeholder="ex. Mr. Anderson"
+                value={this.state.username}
+                onChange={this.handleChange}
               />
             </div>
           </div>
           <div className="col-5">
             <div className="form-group">
-              <label className="label">Телефон</label>
+              <label className="label" htmlFor="phone">Телефон</label>
               <input
+                id="phone"
+                name="phone"
                 className="form-control border__input"
                 type="text"
                 placeholder="ex. 8 999 888 77 66"
+                value={this.state.phone}
+                onChange={this.handleChange}
               />
             </div>
           </div>
@@ -31,19 +68,31 @@ class InputForm extends Component {
         <div className="row col-12">
           <div className="col-7">
             <div className="form-group">
-              <label className="label">E-Mail</label>
+              <label className="label" htmlFor="email">E-Mail</label>
               <input
+                id="email"
+                name="email"
                 className="form-control border__input"
                 type="text"
                 placeholder="ex. example@gmail.com"
+                value={this.state.email}
+                onChange={this.handleChange}
               />
             </div>
           </div>
           <div className="col-5">
-            <button type="button" className="btn-save">
+            <button
+              type="button"
+              className="btn-save"
+              onClick={this.handleSubmit}
+            >
               Сохранить
             </button>
           </div>
+        </div>
+        <div className="row col-12">
+          <i className="fas fa-angle-up add-icon" />
+          <i className="fas fa-angle-up add-icon ml-auto" />
         </div>
       </div>
     );
